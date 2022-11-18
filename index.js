12cf54295eab882669d03885a9502cf98dd92e3c
Eduardo Villaverde <eduardo.villaverde@dcsl.com>
2022-11-18 15:17:48 +0100

    first two challenges

---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+const challenge01 = () => {
+	let nUsrs = 0;
+	let lastUser = {};
+	const { listAcc } = fs.readFileSync(path.join(__dirname, 'inputs', 'users.txt'), 'utf-8')
+		.toString().trim().split('\n')
+		.reduce(({ usrAcc, listAcc }, line) => {
+			if (line === '') {
+				listAcc.push(usrAcc);
+				return { usrAcc: {}, listAcc };
+			} else {
+				const properties = line.split(' ');
+				for (let property of properties) {
+					const [field, value] = property.split(':');
+					usrAcc[field] = value;
+				}
+				return { usrAcc: { ...usrAcc }, listAcc };
+			}
+		}, { usrAcc: {}, listAcc: [] });
+
+
+	listAcc.map((usr) => {
+		if (usr.usr && usr.psw && usr.eme && usr.age && usr.loc && usr.fll) {
+			lastUser = usr;
+			nUsrs++;
+		}
+	});
+
+	console.log(`Result challenge01: ${nUsrs}${lastUser.usr}`);
+}
+
+
+const challenge02 = () => {
+	const words = fs.readFileSync(path.join(__dirname, 'inputs', 'encrypted.txt'), 'utf-8')
+		.toString().trim().split(' ')
+		.map((word) => {
+			const { wordAcc, charAcc } = Array.from(word).reduce(({ charAcc, wordAcc }, nextDigit) => {
+				if (Number.parseInt(charAcc + nextDigit) > 122) {
+					wordAcc.push(Number.parseInt(charAcc));
+					return { charAcc: nextDigit, wordAcc }
+				} else {
+					return { charAcc: charAcc + nextDigit, wordAcc }
+				}
+			}, { charAcc: '', wordAcc: [] })
+			wordAcc.push(Number.parseInt(charAcc));
+			return wordAcc;
+		})
+		.map((word) =>
+			word.map(
+				(char) => String.fromCharCode(char)
+			).reduce((acc, char) => acc + char, '')
+		)
+		.reduce((acc, word) => acc + ' ' + word, '')
+		.trim();
+	console.log('Result challenge02:', words);
+};
